9d550eee52d5c064759bd7b4f2ca7f55195d93a4
Axel <lezinaxel@gmail.com>
2017-01-19 11:55:34 +0100

    Création de la classe Vegetable

---
+++ vegetable.php
@@ -0,0 +1,26 @@
+<?php
+require 'product.php';
+class Vegetable extends Product {
+    private $_productorName;
+    private $_expiresAt;
+
+    public function __construct($productorName, $expiresAt) {
+      $this->setProductorName($productorName);
+      $this->setExpiresAt($expiresAt);
+    }
+    public function isFresh() {
+
+    }
+    public function getProductorName() {
+      return $this->_productorName;
+    }
+    public function setProductorName($productorName) {
+      $this->_productorName = $productorName;
+    }
+    public function getExpiresAt() {
+      return $this->_expiresAt;
+    }
+    public function setExpiresAt($expiresAt) {
+      $this->_expiresAt = $expiresAt;
+    }
+}
